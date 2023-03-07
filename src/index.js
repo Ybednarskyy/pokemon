c194d60b875b8fe9fac12da5b14a770736f34e8f
Ybednarskyy <y.bednarskyy@gmail.com>
2023-03-07 20:13:46 +0200

    makes pokemon card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import { renderMarkup } from './renderMarkup';
 
-fetch('https://pokeapi.co/api/v2/pokemon/1/')
+const pokemonList = document.querySelector('.pokemon-list');
+
+fetch('https://pokeapi.co/api/v2/pokemon/20/')
   .then(response => {
     return response.json();
   })
   .then(pokemon => {
-    console.log(pokemon);
+    renderPokemonCard(pokemon);
   })
   .catch(error => console.log(error));
+
+const renderPokemonCard = ({ name, height, weight, sprites }) => {
+  const markup = `<div class = "card">
+      <img class="card-image" width="auto" src='${sprites.front_default}'
+      alt='${name}' />
+      <h2 class = 'card-title'>Name: ${name}</h2>
+      <p class = 'card-text'>Height: ${height}</p>
+      <p class = 'card-text'>Weight: ${weight}</p>
+    </div>`;
+
+  pokemonList.innerHTML = markup;
+};
